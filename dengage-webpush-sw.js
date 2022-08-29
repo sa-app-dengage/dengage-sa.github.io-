a8dd97a2a45584a3c25fdf5d88aaaa8fea58e04a
sa-app-dengage <112382410+sa-app-dengage@users.noreply.github.com>
2022-08-29 12:14:31 +0300

    Add files via upload
    feat: web push sw

---
+++ dengage-webpush-sw.js
@@ -0,0 +1,8 @@
+
+var swUrl = new URL(location);
+var accountId = swUrl.searchParams.get('account_id') || '';
+var appGuid = swUrl.searchParams.get('app_guid') || '';
+if (accountId && appGuid) {
+    importScripts("https://pcdn.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+}
+        
