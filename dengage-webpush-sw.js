02221e2f42cc54c4f3d42b528ccb16a5690ef4de
sa-app-dengage <112382410+sa-app-dengage@users.noreply.github.com>
2022-08-29 12:27:10 +0300

    Update dengage-webpush-sw.js
    feat: fix web sw

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -3,6 +3,6 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-    importScripts("https://pcdn.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://sa-pcdn.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
         
